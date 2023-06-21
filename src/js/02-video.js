b8ff39876422900620703a9fc2422a8e93c03b27
ANastya28 <guselnikova2803@gmail.com>
2023-06-21 23:56:10 +0300

    Done task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,10 @@
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+// console.log(localStorage);
+// localStorage.setItem('videoplayer-current-time', 'seconds');
+
+// console.log(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,57 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+}
+// console.log(refs.input);
+// console.log(refs.textarea);
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+
+refs.form.addEventListener('submit', throttle(onFormSubmit, 500));
+refs.form.addEventListener('input', onTextareaInput);
+
+
+function onTextareaInput(event) {
+    // console.log(event.target.name);
+    // console.log(event.target.value);
+    
+    formData[event.target.name] = event.target.value;
+    console.log(formData);
+    
+    // const formElements = event.currentTarget.elements; 
+    // console.log(formElements);  
+    // const email = formElements.email.value;   
+    // const message = formElements.message.value;
+    // console.log(mail, password);
+    // const dataToSave = { email, message };
+    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    
+};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const { elements: { email, message } } = event.target;
+    
+    if (email.value === "" || message.value === "" ) {
+        return alert ("Enter your email and password");
+    };
+   
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function getDataFromStorage() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if (savedData) {
+        emailInput.value = savedData.email;
+        textInput.value = savedData.message;
+    }
+}
